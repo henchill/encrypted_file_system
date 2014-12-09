30a9195b6a51d5757818bf6ca40e6f9e3402003d
Quan Nguyen <qmnguyenmw@gmail.com>
2014-12-09 01:21:38 -0500

    Make transmit.py use the with... as... syntax

---
+++ server/transmit-test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2.7
+
+from transmit import EFSConnection
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from encrypt import *
+
+import os
+import sys
+import time
+import json
+import socket
+
+port = 1025
+host = "localhost"
+
+def client():
+	# Test making connection, transmitting
+	test = 6
+	with EFSConnection(host, port) as c:
+		c.transmit(k, "hello")
+	print "TEST %d: PASS" % test
+
+	os._exit(0)
+
+try:
+	# Test key generation
+	test = 0
+	k = RSA.generate(2048)
+	print "TEST %d: PASS" % test
+
+	# Test making server socket
+	test = 1
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	s.bind((host, port))
+	s.listen(2)
+	print "TEST %d: PASS" % test
+
+	newpid = os.fork()
+	Random.atfork()
+	if newpid == 0:
+		time.sleep(2)
+		client()
+
+	# Test accepting connection
+	test = 3
+	conn, addr = s.accept()
+	print "TEST %d: PASS" % test
+
+	# Test receiving short string
+	test = 4
+	data = conn.recv(1024)
+	if data is None or data == "":
+		print "TEST %d: FAIL" % test
+	else:
+		print "TEST %d: PASS" % test
+		# print data
+
+	# Test decryption
+	test = 5
+	d = json.loads(data)
+	plaintext = decrypt(k, [d["payload"]])
+	if plaintext == "hello":
+		print "TEST %d: PASS" % test
+	else:
+		print "TEST %d: FAIL" % test
+
+	time.sleep(2)
+
+	s.close()
+
+except Exception as e:
+	print "TEST %d: EXCEPTION: %s" % (test, str(e))
+	sys.exit(1)
+finally:
+	s.close()
--- server/transmit.py
+++ server/transmit.py
@@ -1,21 +1,50 @@
 import json
+import socket
+
 from encrypt import *
 
-def transmit_ciphertexts(connection, ciphertexts):
-	"""Send an array of ciphertexts through a socket."""
+class EFSConnection:
+	"""Class representing a connection to the server.
 
-	packet_count = len(ciphertexts)
-	packet_seq = 0
+	Use as follows:
 
-	for ciphertext in ciphertexts:
-		packet = {"seq": packet_seq, "count": packet_count, "payload": ciphertext}
-		packet_json = json.dumps(packet)
-		# connection.sendall(packet_json)
-		print packet_json
-		packet_seq += 1
+	with EFSConnection(host, port) as c:
+		c.transmit(key, plaintext)
+	"""
 
-def transmit(connection, key, text):
-	"""Send a string encrypted on key through a socket."""
+	host = None
+	port = None
 
-	ciphertexts = encrypt(key, text)
-	transmit_ciphertexts(connection, ciphertexts)
+	connection = None
+
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+
+	def __enter__(self):
+		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connection.connect((self.host, self.port))
+		return self
+
+	def __exit__(self, type, value, traceback):
+		self.connection.close()
+
+	def transmit_ciphertexts(self, ciphertexts):
+		"""Send an array of ciphertexts through a socket."""
+
+		packet_count = len(ciphertexts)
+		packet_seq = 0
+
+		for ciphertext in ciphertexts:
+			packet = {"seq": packet_seq, "count": packet_count, "payload": ciphertext}
+			packet_json = json.dumps(packet)
+			self.connection.sendall(packet_json)
+			# print packet_json
+			packet_seq += 1
+
+	def transmit(self, key, text):
+		"""Send a string encrypted on key through a socket."""
+
+		ciphertexts = encrypt(key, text)
+		self.transmit_ciphertexts(ciphertexts)
+
